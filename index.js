46cfb17838504ab4706b2ca0f6a657c90852f1d5
Anton Zhuravlev <anton.zhuravlev21@gmail.com>
2022-08-11 19:21:57 +0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   MERN
    	modified:   ReadMe
    	modified:   index.js
    	modified:   models/User.js

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ mongoose
 
 
 
-
-
 const app = express();
 //создаем использования логики express в формате json
 app.use(express.json());
@@ -32,40 +30,68 @@ app.use(express.json());
 //   res.send("Hello World!");
 // });
 
+
+
+
+//Регистрация пользователя с ловлей ошибок  
+app.post('/auth/register', registerValidation, async (req, res) =>
+{
+  //Отправляем запрос на '/auth/register' проверяем есть ли там registerValidation, если есть то выполняем функцию дальше
+  try {
+    //Вытаскиваем все из запроса и передаем все ошибки
+    const errors = validationResult(req);
+    //Если все же ошибки есть, то возвращаем ответ 400
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    //шифровка пароля и соль
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+    //Сохраняем документ
+    const user = await doc.save();
+
+    //Пишем токен и удаление его через 30 дней
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    //С помощью деструктиризации вытаскиваем и не используем хеш пароля
+    const { passwordHash, ...userData } = user._doc;
+
+    //вытаскиваем только документ без пароля из БД
+    res.json({
+      ...userData,
+      token,
+    });
+
+    //Выдаем 500 ошибку
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      massage: "Не удалось зарегистрироваться",
+    });
+  }
+});
+
 app.listen(4444, (err) => {
   if (err) {
     return console.log(err);
   }
 
   console.log("Server OK");
-});
-
-//Отправляем запрос на '/auth/register' проверяем есть ли там registerValidation, если есть то выполняем функцию дальше 
-app.post('/auth/register', registerValidation,async (req, res) => {
-    //Вытаскиваем все из запроса и передаем все ошибки 
-    const errors = validationResult(req);
-    //Если все же ошибки есть, то возвращаем ответ 400
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
-    }
-     
-
-    //шифровка пароля и соль
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    });
-
-
-    const user = await doc.save();
-
-    res.json({
-        success: true,
-    });
 });
--- models/User.js
+++ models/User.js
@@ -4,26 +4,27 @@ import mongoose from "mongoose";
 //Создаем МОДЕЛЬ пользователя 
 
 //Все свойства для пользователя 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     fullName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     passwordHash: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     avatarUrl: String,
-},
-    //Указываем, что при создании пользователя прикручивается дата создания 
-    {
-        timestamps: true,
-    },
+  },
+  //Указываем, что при создании пользователя прикручивается дата создания
+  {
+    timestamps: true,
+  }
 );
 
 export default mongoose.model('User', UserSchema);
